a10049d4260856bcde72826303733c935072d7d6
yekkong <chloeseo4837@gmail.com>
2022-05-11 16:06:56 +0900

    Create 1007_출력하기07.java

---
+++ CodeUp/1007_출력하기07.java
@@ -0,0 +1,15 @@
+/*
+	1007 - 출력하기07
+
+	윈도우 운영체제의 파일 경로를 출력하는 연습을 해보자.
+	파일 경로에는 특수문자들이 포함된다.
+	다음 경로를 출력하시오.
+	"C:\Cownload\hello.cpp"
+	(단, 큰따옴표도 함께 출력한다.)
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+}
