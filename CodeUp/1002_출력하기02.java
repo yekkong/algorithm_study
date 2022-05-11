2f619b21645677843f275ee683a6683439c432ee
yekkong <chloeseo4837@gmail.com>
2022-05-11 14:55:37 +0900

    Create 1002_출력하기02.java

---
+++ CodeUp/1002_출력하기02.java
@@ -0,0 +1,13 @@
+/*
+	1002 - 출력하기02
+
+	이번에는 공백을 포함한 문장을 출력한다. 다음 문장을 출력해보자.
+	Hello World
+	(대소문자에 주의한다.)
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
