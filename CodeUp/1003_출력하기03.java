88fd53b9411bdfff9efe7f0b35340bb5b2134a5c
yekkong <chloeseo4837@gmail.com>
2022-05-11 14:59:26 +0900

    Create 1003_출력하기03.java

---
+++ CodeUp/1003_출력하기03.java
@@ -0,0 +1,15 @@
+/*
+	1003 - 출력하기03
+
+	이번에는 줄을 바꿔 출력하는 출력문을 연습해보자. 다음과 같이 줄을 바꿔 출력해야 한다.
+	Hello
+	World
+	(두 줄에 걸쳐 줄을 바꿔 출력)
+*/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+	}
+}
