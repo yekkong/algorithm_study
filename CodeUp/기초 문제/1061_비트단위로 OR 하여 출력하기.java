156fac39d15e9ec6c6d6e98e5bf30ffeb4f8d412
yekkong <chloeseo4837@gmail.com>
2022-05-31 11:00:28 +0900

    Create 1061_비트단위로 OR 하여 출력하기.java

---
+++ CodeUp/기초 문제/1061_비트단위로 OR 하여 출력하기.java
@@ -0,0 +1,20 @@
+/*
+	1061 - 비트단위로 OR 하여 출력하기
+
+	입력된 정수 두 개를 비트단위로 or 연산한 후 그 결과를 정수로 출력해보자.
+
+	힌트: 비트단위(bitwise) 연산자 |를 사용하면 된다.
+
+	예) 3 5 -> 7
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a|b); // | is bitwise OR whereas || is the logical operator OR.
+	}
+}
